938a79c06fdef5af58f747bcd6084878c4dfe7ea
WalStar29 <castrowalter172913@hotmail.com>
2025-01-19 19:47:25 -0400

    first commit

---
+++ src/data/logos.js
@@ -0,0 +1,67 @@
+const logos = [
+    {   
+        name: "Node JS",
+        imagen: "/public/Logos/nodejs.png"
+    },
+
+    {   
+        name: "Astro",
+        imagen: "/public/Logos/astro.png",
+    },
+
+    {   
+        name: "React",
+        imagen: "/public/Logos/react.png",
+    },
+
+    {   
+        name: "HTML",
+        imagen: "/public/Logos/html.png"
+    },
+
+    {   
+        name: "CSS",
+        imagen: "/public/Logos/css.png"
+    },
+
+    {   
+        name: "JS",
+        imagen: "/public/Logos/JavaScript.png"
+    },
+
+    {   
+        name: "C",
+        imagen: "/public/Logos/c.png"
+    },
+
+    {   
+        name: "C++",
+        imagen: "/public/Logos/c++.png"
+    },
+
+    {   
+        name: "PHP",
+        imagen: "/public/Logos/php.png"
+    },
+
+    {   
+        name: "Python",
+        imagen: "/public/Logos/python.png"
+    },
+
+    {   
+        name: "MySql Server",
+        imagen: "/public/Logos/mysql.png"
+    },
+
+    {   
+        name: "Git",
+        imagen: "/public/Logos/git.png"
+    },
+
+    {   
+        name: "Github",
+        imagen: "/public/Logos/github.png"
+    },
+];
+export default logos
---
+++ src/data/proyectos.js
@@ -0,0 +1,12 @@
+const proyectos = [
+
+    {   
+        name: "Portal Digital Informativo",
+        imagen: "/public/Proyectos/Nueva Esparta.png",
+        ulr: "https://walstar29.github.io/Portal-Digital/",
+        ulr2: "https://github.com/WalStar29/Portal-Digital",
+        descripcion: "Portal Digital que beneficira a la comunidad del municipio tubores ubicada en Nueva Esparta. Tiene como finalidad profundizar la informacion de la zona dando y ayudando a la comunidad a resolver problemas locales "
+    },
+];
+
+export default proyectos
