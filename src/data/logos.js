a4a2b0dcbe04e693a1b19492cf7ddf1bd7315bf1
WalStar29 <castrowalter172913@hotmail.com>
2025-01-19 20:12:07 -0400

    ajuste de imagen

--- src/data/logos.js
+++ src/data/logos.js
@@ -1,67 +1,67 @@
 const logos = [
     {   
         name: "Node JS",
-        imagen: "/public/Logos/nodejs.png"
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/nodejs.png?raw=true"
     },
 
     {   
         name: "Astro",
-        imagen: "/public/Logos/astro.png",
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/astro.png?raw=true",
     },
 
     {   
         name: "React",
-        imagen: "/public/Logos/react.png",
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/react.png?raw=true",
     },
 
     {   
         name: "HTML",
-        imagen: "/public/Logos/html.png"
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/html.png?raw=true"
     },
 
     {   
         name: "CSS",
-        imagen: "/public/Logos/css.png"
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/css.png?raw=true"
     },
 
     {   
         name: "JS",
-        imagen: "/public/Logos/JavaScript.png"
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/JavaScript.png?raw=true"
     },
 
     {   
         name: "C",
-        imagen: "/public/Logos/c.png"
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/c.png?raw=true"
     },
 
     {   
         name: "C++",
-        imagen: "/public/Logos/c++.png"
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/c++.png?raw=true"
     },
 
     {   
         name: "PHP",
-        imagen: "/public/Logos/php.png"
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/php.png?raw=true"
     },
 
     {   
         name: "Python",
-        imagen: "/public/Logos/python.png"
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/python.png?raw=true"
     },
 
     {   
         name: "MySql Server",
-        imagen: "/public/Logos/mysql.png"
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/mysql.png?raw=true"
     },
 
     {   
         name: "Git",
-        imagen: "/public/Logos/git.png"
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/git.png?raw=true"
     },
 
     {   
         name: "Github",
-        imagen: "/public/Logos/github.png"
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Logos/github.png?raw=true"
     },
 ];
 export default logos
--- src/data/proyectos.js
+++ src/data/proyectos.js
@@ -1,12 +1,18 @@
 const proyectos = [
+    {   
+        name: "Portafolio (Personal) ",
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/me.jpg?raw=true",
+        ulr2: "https://github.com/WalStar29/My-Portfolio",
+        descripcion: " Un portafolio que muestra mis habilidades en la que a sido desarrollado con React. Muetras los proyectos que se han desarrollado con variedades de tecnologias. Este portafolio es un replica del desarrollo original basado en Astro"
+    },
 
     {   
         name: "Portal Digital Informativo",
-        imagen: "/public/Proyectos/Nueva Esparta.png",
+        imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Proyectos/Nueva%20Esparta.png?raw=true",
         ulr: "https://walstar29.github.io/Portal-Digital/",
         ulr2: "https://github.com/WalStar29/Portal-Digital",
         descripcion: "Portal Digital que beneficira a la comunidad del municipio tubores ubicada en Nueva Esparta. Tiene como finalidad profundizar la informacion de la zona dando y ayudando a la comunidad a resolver problemas locales "
     },
-];
+]
 
 export default proyectos
