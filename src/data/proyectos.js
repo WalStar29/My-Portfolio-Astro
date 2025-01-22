9e31a26ec76784384383499fabd5676daf3c8ff7
WalStar29 <castrowalter172913@hotmail.com>
2025-01-22 00:02:17 -0400

    cambio de enlaces

--- src/data/proyectos.js
+++ src/data/proyectos.js
@@ -2,15 +2,16 @@ const proyectos = [
     {   
         name: "Portafolio (Personal) ",
         imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/me.jpg?raw=true",
+        ulr:"",
         ulr2: "https://github.com/WalStar29/My-Portfolio",
         descripcion: " Un portafolio que muestra mis habilidades en la que a sido desarrollado con React. Muetras los proyectos que se han desarrollado con variedades de tecnologias. Este portafolio es un replica del desarrollo original basado en Astro"
     },
 
     {   
-        name: "Portal Digital Informativo",
+        name: "Portal Educativo Ambiental",
         imagen: "https://github.com/WalStar29/My-Portfolio-Astro/blob/main/public/Proyectos/Nueva%20Esparta.png?raw=true",
-        ulr: "https://walstar29.github.io/Portal-Digital/",
-        ulr2: "https://github.com/WalStar29/Portal-Digital",
+        ulr: "https://walstar29.github.io/Portal-Educativo-Ambiental/",
+        ulr2: "https://github.com/WalStar29/Portal-Educativo-Ambiental",
         descripcion: "Portal Digital que beneficira a la comunidad del municipio tubores ubicada en Nueva Esparta. Tiene como finalidad profundizar la informacion de la zona dando y ayudando a la comunidad a resolver problemas locales "
     },
 ]
